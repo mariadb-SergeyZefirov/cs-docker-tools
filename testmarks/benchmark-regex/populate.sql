010e141e18d07f5817b7e1488eac43640f7278dd
Sergey Zefirov <serguey.zefirov@mariadb.com>
2020-11-05 15:50:25 +0300

    More cases

--- testmarks/benchmark-regex/populate.sql
+++ testmarks/benchmark-regex/populate.sql
@@ -6,14 +6,14 @@
 -------------------------------------------------------------------------------
 -- a table with text lines. it contains number of delimiters used and text
 -- with the specified number of delimiters.
-CREATE TABLE IF NOT EXISTS texts (num_delims INTEGER, text_line TEXT);
+CREATE TABLE IF NOT EXISTS texts (num_delims INTEGER NOT NULL, text_line TEXT NOT NULL);
 
 DELETE FROM texts;
 
 -- it was once temporary but I decided to make it
 -- a part of testing. We can use these texts to construct
 -- queries identical to the ones stored in texts to, well, check everything.
-CREATE TABLE IF NOT EXISTS text_parts (text_part TEXT);
+CREATE TABLE IF NOT EXISTS text_parts (text_part TEXT NOT NULL);
 DELETE FROM text_parts;
 
 INSERT INTO text_parts (text_part) VALUES ("fiyulnabmi");
@@ -21,6 +21,8 @@ INSERT INTO text_parts (text_part) VALUES ("noinceolik");
 INSERT INTO text_parts (text_part) VALUES ("gesundheit");
 INSERT INTO text_parts (text_part) VALUES ("bebegonebe");
 INSERT INTO text_parts (text_part) VALUES ("not ten chars");
+INSERT INTO text_parts (text_part) VALUES ("bigger than thirteen chars");
+INSERT INTO text_parts (text_part) VALUES ("but less than one hundred eighty eight chars long");
 INSERT INTO text_parts (text_part) VALUES ("zurbagan");
 INSERT INTO text_parts (text_part) VALUES ("русский текст");
 INSERT INTO text_parts (text_part) VALUES ("if a packet hits a pocket on a socket on a port");
@@ -45,15 +47,18 @@ INSERT INTO texts (num_delims, text_line)
 
 INSERT INTO texts (num_delims, text_line)
     SELECT 1, 'START' || T0.text_part || '1' || T1.text_part || 'END'
-    FROM text_parts as T0, text_parts as T1;
+    FROM text_parts as T0, text_parts as T1
+    WHERE T0.text_part <> T1.text_part;
 
 INSERT INTO texts (num_delims, text_line)
     SELECT 2, 'START' || T0.text_part || '1' || T1.text_part || '2' || T2.text_part || 'END'
-    FROM text_parts as T0, text_parts as T1, text_parts as T2;
+    FROM text_parts as T0, text_parts as T1, text_parts as T2
+    WHERE T0.text_part <> T1.text_part AND T1.text_part <> T2.text_part;
 
 INSERT INTO texts (num_delims, text_line)
     SELECT 3, 'START' || T0.text_part || '1' || T1.text_part || '2' || T2.text_part || '3' || T3.text_part || 'END'
-    FROM text_parts as T0, text_parts as T1, text_parts as T2, text_parts as T3;
+    FROM text_parts as T0, text_parts as T1, text_parts as T2, text_parts as T3
+    WHERE T0.text_part <> T1.text_part AND T1.text_part <> T2.text_part AND T2.text_part <> T3.text_part;
 
 SELECT COUNT(*) FROM texts;
 
