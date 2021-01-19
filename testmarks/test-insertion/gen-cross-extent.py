48bf0ac580b96f2665d666c82d1798390eb09fc5
root <root@sergueyz-devel-1.c.columnstore-test.internal>
2021-01-19 19:48:33 +0000

    test-insert/gen-cross-extent.py produces error I wanted to see

---
+++ testmarks/test-insertion/gen-cross-extent.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print ("use test;")
+print ("create table t (x integer) engine = columnstore;")
+beginning8K = "insert into t(x) values " + ("(1)," * 8191) + "(1);"
+endAlmost8K = "insert into t(x) values " + ("(1)," * 8190) + "(1);"
+threeToCrossExtent = "insert into t(x) values (42), (43), (44);"
+for _ in range(0,1023):
+    print(beginning8K)
+print(endAlmost8K)
+print(threeToCrossExtent)
+
