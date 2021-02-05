b5493eea7eb202e46e0aacfc6c5d943287f6c34d
Sergey Zefirov <serguey.zefirov@mariadb.com>
2021-02-05 16:36:36 +0300

    Fix for delete tests
    They were tests for updates, now they are true delete tests.

--- testmarks/range-book-keeping/test-deletes/test-delete-keeps-invalid-range.sql
+++ testmarks/range-book-keeping/test-deletes/test-delete-keeps-invalid-range.sql
@@ -2,6 +2,6 @@
 use test;
 drop table if exists t;
 create table t(x integer) engine=columnstore;
-insert into t(x) values (44),(55),(66); -- range must be 44..66.
-update t set x=65 where x=66; -- range must drop to invalid
-update t set x=65 where x=55; -- range must stay invalid.
+insert into t(x) values (44),(50),(55),(66); -- range must be 44..66.
+delete from t where x=66; -- range must drop to invalid
+delete from t where x=50; -- range must stay invalid.
