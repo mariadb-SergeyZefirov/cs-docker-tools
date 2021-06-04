c3dcfdc7a6471b0f9937d1cc8c93d885d988cace
root <root@sergueyz-devel-1.c.columnstore-test.internal>
2021-06-04 13:35:15 +0000

    Move out of centos

--- shells/BRM_cont.sh
+++ shells/BRM_cont.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gdb /usr/local/mariadb/columnstore/bin/controllernode --tui -p $(pgrep -f /usr/local/mariadb/columnstore/bin/controllernode)
+#gdb /usr/local/mariadb/columnstore/bin/controllernode --tui -p $(pgrep -f /usr/local/mariadb/columnstore/bin/controllernode)
+gdb /usr/bin/controllernode --tui -p $(pgrep -f /usr/bin/controllernode)
--- shells/exemgr_gdb.sh
+++ shells/exemgr_gdb.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gdb /usr/bin/ExeMgr --tui -p $(pidof ExeMgr)
+#gdb /usr/bin/ExeMgr --tui -p $(pidof ExeMgr)
+gdb /usr/bin/ExeMgr -p $(pidof ExeMgr)
